22e3b6b278ada223f7c65abb4c07bcbe90a32228
myunusseker <m.yunusseker@hotmail.com>
2019-02-27 12:58:10 +0100

    ASC launch file created

--- scripts/effect_marker_client.py
+++ scripts/effect_marker_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import math
 import rospy
 from imagine_common.srv import EffectMarker
 from geometry_msgs.msg import Pose
@@ -18,9 +19,9 @@ def effect_marker_client(pose):
 if __name__ == "__main__":
     pose = Pose()
     pose.position.x = 0.559
-    pose.position.y = -0.006
+    pose.position.y = -0.086
     pose.position.z = 0.002
-    r = tf.transformations.quaternion_from_euler(0, 0, 0)
+    r = tf.transformations.quaternion_from_euler(0, 0, (-90/180.)*math.pi)
     pose.orientation.x = r[0]
     pose.orientation.y = r[1]
     pose.orientation.z = r[2]
