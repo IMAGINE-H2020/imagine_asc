ae106a7f4d145d55af4a8fa686d4aab377f4d1b5
ahmet <tekdenahmet@gmail.com>
2019-11-11 19:16:00 +0300

    Fixed spacing

--- scripts/screwability.py
+++ scripts/screwability.py
@@ -22,50 +22,49 @@ class Screwability:
         markerArray= MarkerArray()
         no = 0    
         for part in data.part_array:
-        partname=part.part_id[:-1]
-        marker= Marker()
-        if partname=='screw' or partname=='bearing':
-            aff=Affordance()
-            aff.object_name=part.part_id
-            aff.effect_name='unscrewable'
-            aff.affordance_name='unscrewability'
-            ap= ActionParameters()
-            if (partname == 'bearing'):
-                ap.confidence = part.part_type_specifics_confidence  
-            else:
-                ap.confidence = part.part_type_confidence                                      
-            asc_pair =AscPair()
-            asc_pair.key='start_pose'
-            asc_pair.value_type=2
-            asc_pair.value_pose=part.pose
-            ap.parameters.append(asc_pair)
-            aff.action_parameters_array.append(ap)
-            aff_list.append(aff)
-        
-        #marker here
-        h = std_msgs.msg.Header()
-        h.stamp = rospy.Time.now()
-        h.frame_id="world"
-        marker.header=h
-        marker.ns= "unscrew_points"
-        no = no + 1
-        marker.id = no 
-        marker.type= 3 
-        marker.action = 0
-        marker.scale.x=0.005
-        marker.scale.y=0.005
-        marker.scale.z=0.005
-        marker.lifetime = rospy.Duration(90)
-        marker.color.r=0.0
-        marker.color.g=0.0
-        marker.color.b=1.0
-        marker.color.a=1.0
-        marker.pose=part.pose
-        markerArray.markers.append(marker)
-        aff.action_parameters_array.append(ap)
-        
-    rate=rospy.Rate(10)
-    for _ in range(5):
-        self.unscrew_rviz.publish(markerArray)
-        rate.sleep()
-    return aff_list
+            partname=part.part_id[:-1]
+            if partname=='screw' or partname=='bearing':
+                marker= Marker()
+                aff=Affordance()
+                aff.object_name=part.part_id
+                aff.effect_name='unscrewable'
+                aff.affordance_name='unscrewability'
+                ap= ActionParameters()
+                if (partname == 'bearing'):
+                    ap.confidence = part.part_type_specifics_confidence  
+                else:
+                    ap.confidence = part.part_type_confidence                                      
+                asc_pair =AscPair()
+                asc_pair.key='start_pose'
+                asc_pair.value_type=2
+                asc_pair.value_pose=part.pose
+                ap.parameters.append(asc_pair)
+                aff.action_parameters_array.append(ap)
+                aff_list.append(aff)
+
+                #marker here
+                h = std_msgs.msg.Header()
+                h.stamp = rospy.Time.now()
+                h.frame_id="world"
+                marker.header=h
+                marker.ns= "unscrew_points"
+                no = no + 1
+                marker.id = no 
+                marker.type= 3 
+                marker.action = 0
+                marker.scale.x=0.005
+                marker.scale.y=0.005
+                marker.scale.z=0.005
+                marker.lifetime = rospy.Duration(90)
+                marker.color.r=0.0
+                marker.color.g=0.0
+                marker.color.b=1.0
+                marker.color.a=1.0
+                marker.pose=part.pose
+                markerArray.markers.append(marker)
+                aff.action_parameters_array.append(ap)
+        rate=rospy.Rate(10)
+        for _ in range(5):
+            self.unscrew_rviz.publish(markerArray)
+            rate.sleep()
+        return aff_list
